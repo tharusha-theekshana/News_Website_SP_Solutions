e521fee2985258d0b6c29b8ea93911a69979a962
Theekshana <theenutheekshana@gmail.com>
2025-03-28 10:11:12 +0530

    Initial commit

---
+++ src/test/java/com/sp_solutions/news_website/NewsWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp_solutions.news_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
